1b7fd81236d12f3b11edc65ddf07aee1f8e3315b
DalmeGNU <dalmemail@gmail.com>
2016-11-13 20:49:34 +0100

    Add "2_build_kernel.sh"
    This script should compile the Linux-libre kernel

---
+++ 2_build_kernel.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export ARCH=i386
+export CFLAGS=-m32
+export CPPFLAGS=-m32
+export LDFLAGS=-m32
+
+cd work/kernel
+
+# Change to the first directory ls finds, e.g. 'linux-3.18.6'
+cd $(ls -d *)
+
+# Patch for ldlinux
+patch -p1 < ../../../ldlinux.patch
+
+# Cleans up the kernel sources, including configuration files
+make mrproper
+
+# Create a default configuration file for the kernel
+make defconfig
+
+# Changes the name of the system
+sed -i "s/.*CONFIG_DEFAULT_HOSTNAME.*/CONFIG_DEFAULT_HOSTNAME=\"minimal\"/" .config
+
+# Compile the kernel with optimization for "parallel jobs" = "number of processors"
+# Good explanation of the different kernels
+# http://unix.stackexchange.com/questions/5518/what-is-the-difference-between-the-following-kernel-makefile-terms-vmlinux-vmlinux
+make bzImage -j $(grep ^processor /proc/cpuinfo | wc -l)
+
+cd ../../..
